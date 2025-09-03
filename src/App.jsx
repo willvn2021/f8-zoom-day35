b9cba32c66ba0e56ea8fdeaf483b854be49ec9d2
F8 <williamlrasmussen@gmail.com>
2025-09-03 12:34:42 +0700

    Update from deploy script

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import AppRoutes from "./components/AppRoutes";
-import "./App.css";
-
-function App() {
-    return <AppRoutes />;
-}
-
-export default App;
--- src/pages/Home/index.jsx
+++
@@ -1,5 +0,0 @@
-function Home() {
-    return <h1>Chào mừng đến với F8 React Day 35</h1>;
-}
-
-export default Home;
